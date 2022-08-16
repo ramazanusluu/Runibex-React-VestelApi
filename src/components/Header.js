8dee8f9613d15159e8be63bc58fd187e16ec32cf
Ramazan USLU <ruslu1720@gmail.com>
2022-08-16 19:33:39 +0300

    Header component'i eklendi

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import headerLogo from "../image/header-logo.png";
+
+function Header() {
+  return (
+    <div>
+      <ul className="nav justify-content-center">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">
+            <img src={headerLogo} alt="logo" title="vestel-logo" style={{width: "140px"}}/>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Header;
