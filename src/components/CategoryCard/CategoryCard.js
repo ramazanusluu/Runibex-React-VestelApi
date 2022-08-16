8e634c5b3ecae1acfd5aea8304f1d0123ac55506
Ramazan USLU <ruslu1720@gmail.com>
2022-08-17 02:43:11 +0300

    Ana syafada yer alan Category listesi duzenlendi

--- src/components/CategoryCard/CategoryCard.js
+++ src/components/CategoryCard/CategoryCard.js
@@ -1,9 +1,17 @@
 import React from "react";
+import "./CategoryCard.css";
 
 function CategoryCard({ item }) {
   return (
     <>
-      <div className="col-lg-4">{item.DisplayName}</div>
+      <div className="col-sm-6 col-lg-4 col-xl-3">
+        <div className="card mt-5">
+          <div className="card-head">
+            <img src={item.ImageUri} className="card-img-top" alt={item.Code} />
+          </div>
+        </div>
+        <div className="card-text title my-3 text-center">{item.DisplayName}</div>
+      </div>
     </>
   );
 }
