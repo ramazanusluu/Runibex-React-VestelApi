b8e3d19f4d528517d36b4992014c2389fd4a1263
Ramazan USLU <ruslu1720@gmail.com>
2022-08-17 03:44:15 +0300

    Sidebar uzerinde category listeleme eklendi

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,20 +1,44 @@
 import React from "react";
 import "./Sidebar.css";
+import Loading from "../Loading/Loading";
+import { useQuery } from "react-query";
+import { fetchCategoryList } from "../../api";
 
 function Sidebar() {
+  const { isLoading, error, data } = useQuery("category", fetchCategoryList);
+
+  if (isLoading) return <Loading />;
+
+  if (error) return "An error has occurred: " + error.message;
+
+  console.log("Sidebar", data.Result.TreeList);
   return (
     <div className="container">
       <div className="sidebar">
+        {data.Result.TreeList.map((item, key) => (
+          <ul key={key}>
+            <li>
+              <button className="btn btn-danger category">
+                {item.DisplayName}
+              </button>
+              {item.SubCategoryList.map((item2, key2) => (
+                <ul key={key2}>
+                  <li>
+                    <button className="btn btn-danger subCategory">
+                      {item2.DisplayName}
+                    </button>
+                  </li>
+                </ul>
+              ))}
+            </li>
+          </ul>
+        ))}
         <ul>
           <li>
-            <a href="/" className="category">
-              Televizyon
-            </a>
+            <button className="btn btn-danger category">Televizyon</button>
             <ul>
               <li>
-                <a href="/" className="subCategory">
-                  Smart
-                </a>
+                <button className="btn btn-danger subCategory">Smart</button>
               </li>
             </ul>
           </li>
