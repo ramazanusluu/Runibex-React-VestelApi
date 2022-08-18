e22b8ee2a53c09bf54d8f790aee7f0303befda2b
Ramazan USLU <ruslu1720@gmail.com>
2022-08-18 11:06:12 +0300

    Product Detail sayfasi hazirlandi

--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -4,6 +4,7 @@ import { useQuery } from "react-query";
 import Loading from "../Loading/Loading";
 import { fetchProductDetail } from "../../api";
 import ImageGallery from "react-image-gallery";
+import "./ProductDetail.css"
 
 function ProductDetail() {
   const { product } = useParams();
@@ -20,17 +21,52 @@ function ProductDetail() {
   const images = data.Result.ImageSetList[0].ImageList.map((url) => ({
     original: url.Path,
   }));
+  const item = data.Result;
 
   return (
     <div>
       {/* <h1>Product Detail - {product}</h1> */}
       <div className="container my-5">
         <div className="row">
-          <div className="col-md-6">
+          <div className="col-xl-6">
             <ImageGallery items={images} showThumbnails={true} />
           </div>
-          <div className="col-md-6">
-            <h1>resim</h1>
+          <div className="col-xl-6">
+            <div>
+              <h1 className="">{item.DisplayName}</h1>
+              <div className="fs-5 my-2">
+                <i className="fa-solid fa-caret-right text-danger"></i>
+                <span className="text-muted ms-3">
+                  {item.VisibleAttributeList[0].DisplayName}
+                </span>
+              </div>
+              <div className="fs-5 my-2">
+                <i className="fa-solid fa-caret-right text-danger"></i>
+                <span className="text-muted ms-3">
+                  {item.VisibleAttributeList[1].DisplayName}
+                </span>
+              </div>
+              <div className="fs-5 my-2">
+                <i className="fa-solid fa-caret-right text-danger"></i>
+                <span className="text-muted ms-3">
+                  {item.VisibleAttributeList[2].DisplayName}
+                </span>
+              </div>
+              <div className="fs-5 my-2">
+                <i className="fa-solid fa-caret-right text-danger"></i>
+                <span className="text-muted ms-3">
+                  {item.VisibleAttributeList[3].DisplayName}
+                </span>
+              </div>
+              <div className="fs-5 my-2">
+                <i className="fa-solid fa-caret-right text-danger"></i>
+                <span className="text-muted ms-3">
+                  {item.VisibleAttributeList[4].DisplayName}
+                </span>
+              </div>
+              <h2 className="text-danger my-5">{item.ActualPriceToShowOnScreen} TL</h2>
+              <button type="button" className="basket">SEPETE EKLE</button>
+            </div>
           </div>
         </div>
       </div>
