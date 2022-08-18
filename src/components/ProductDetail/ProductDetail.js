09f9edaac1442de6752904e1d365625415a7fc24
Ramazan USLU <ruslu1720@gmail.com>
2022-08-18 09:23:37 +0300

    Product Detail sayfasi icin react-image-gallery eklendi

--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { useQuery } from "react-query";
 import Loading from "../Loading/Loading";
 import { fetchProductDetail } from "../../api";
+import ImageGallery from "react-image-gallery";
 
 function ProductDetail() {
   const { product } = useParams();
@@ -16,9 +17,23 @@ function ProductDetail() {
 
   console.log(data);
 
+  const images = data.Result.ImageSetList[0].ImageList.map((url) => ({
+    original: url.Path,
+  }));
+
   return (
     <div>
-      <h1>Product Detail - {product}</h1>
+      {/* <h1>Product Detail - {product}</h1> */}
+      <div className="container my-5">
+        <div className="row">
+          <div className="col-md-6">
+            <ImageGallery items={images} showThumbnails={true} />
+          </div>
+          <div className="col-md-6">
+            <h1>resim</h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
